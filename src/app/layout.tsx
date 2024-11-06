c006b67b78665791eb7ad475bd1fc48deabdf90f
Gavin Arori <99688077+gavinarori@users.noreply.github.com>
2024-11-06 12:29:14 +0300

    Porfolio (#6)
    * security for CRUD
    
    * timeline
    
    * added content
    
    * added content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,47 +11,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
-    default: siteConfig.name,
+    default: '',
     template: `%s | ${siteConfig.name}`,
   },
   description: siteConfig.description,
-  keywords: [
-    "Next.js",
-    "React",
-    "Tailwind CSS",
-    "Server Components",
-    "Radix UI",
-    "Shadcnui",
-    "Javascript",
-    "Blog",
-  ],
-  authors: [
-    {
-      name: "Coding Jitsu",
-      url: "https://github.com/codingjitsu",
-    },
-  ],
-  creator: "coding jitsu",
-  openGraph: {
-    type: "website",
-    locale: "en_US",
-    url: siteConfig.url,
-    title: siteConfig.name,
-    description: siteConfig.description,
-    siteName: siteConfig.name,
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: siteConfig.name,
-    description: siteConfig.description,
-    images: [`${siteConfig.url}/og`],
-    creator: "@codingJitsu",
-  },
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "apple-touch-icon.png",
-  },
 };
 
 export default function RootLayout({
